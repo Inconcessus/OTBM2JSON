00fdb4f365cba305ef86c08e2d496eb31adcc577
Inconcessus <38814434+Inconcessus@users.noreply.github.com>
2018-05-01 12:53:37 +0200

    Add forest example

---
+++ examples/forest/README.md
@@ -0,0 +1,9 @@
+# Example Forest
+Generates random trees on some grass tiles
+
+<p align="center">
+  <img src="grass.png">
+  <img src="/images/convert.png">
+  <img src="forest.png">
+</p>
+
---
+++ examples/forest/example.js
@@ -0,0 +1,59 @@
+/*
+ * Example script of using the otbm2json library
+ * Changes all tiles on a map to chessboard pattern in global coordinates
+ */
+
+const otbm2json = require("../../otbm2json");
+
+const BLACK_TILE = 407;
+const WHITE_TILE = 406;
+
+// Read the map data using the otbm2json library
+const mapData = otbm2json.read("grass.otbm");
+
+function randomTree() {
+
+  /* function randomTree
+   * Returns an identifier of a random tree
+   */
+
+  var trees = [
+    2701,
+    2702,
+    2703,
+    2704,
+    2705,
+    2706
+  ];
+
+  return trees[Math.floor(Math.random() * trees.length)];
+
+}
+
+// Go over all nodes
+mapData.data.nodes.forEach(function(x) {
+
+  x.features.forEach(function(x) {
+
+    if(x.type !== "OTBM_TILE_AREA") return; 
+
+    x.tiles.forEach(function(x) {
+
+      if(x.type !== "OTBM_TILE") return; 
+
+      // On 1/4th of all tiles put a random tree
+      if(Math.random() < 0.25) {
+        x.items = [{ 
+          "type": "OTBM_ITEM",
+          "id": randomTree()
+        }];
+      }
+
+    });
+
+  });
+
+});
+
+// Write the output to OTBM using the library
+otbm2json.write("forest.otbm", mapData);
