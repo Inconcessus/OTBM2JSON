684fe14178565ed00642925628ae00e075ecf1ff
Forby <38814434+Inconcessus@users.noreply.github.com>
2018-05-17 11:25:07 +0200

    Memory reduction: use int instead of str

--- otbm2json.js
+++ otbm2json.js
@@ -1,46 +1,11 @@
 const fs = require("fs");
-
-// Magic bytes
-const OTBM_MAP_HEADER = 0x00;
-const OTBM_MAP_DATA = 0x02;
-const OTBM_TILE_AREA = 0x04;
-const OTBM_TILE = 0x05;
-const OTBM_ITEM = 0x06;
-const OTBM_TOWNS = 0x0C;
-const OTBM_TOWN = 0x0D;
-const OTBM_HOUSETILE = 0x0E;
-const OTBM_WAYPOINTS = 0x0F;
-const OTBM_WAYPOINT = 0x10;
-
-const OTBM_ATTR_DESCRIPTION = 0x01;
-const OTBM_ATTR_EXT_FILE = 0x02;
-const OTBM_ATTR_TILE_FLAGS = 0x03;
-const OTBM_ATTR_ACTION_ID = 0x04;
-const OTBM_ATTR_UNIQUE_ID = 0x05;
-const OTBM_ATTR_TEXT = 0x06;
-const OTBM_ATTR_DESC = 0x07;
-const OTBM_ATTR_TELE_DEST = 0x08;
-const OTBM_ATTR_ITEM = 0x09;
-const OTBM_ATTR_DEPOT_ID = 0x0A;
-const OTBM_ATTR_EXT_SPAWN_FILE = 0x0B;
-const OTBM_ATTR_EXT_HOUSE_FILE = 0x0D;
-const OTBM_ATTR_HOUSEDOORID = 0x0E;
-const OTBM_ATTR_COUNT = 0x0F;
-const OTBM_ATTR_RUNE_CHARGES = 0x16;
-
-const TILESTATE_NONE = 0x0000;
-const TILESTATE_PROTECTIONZONE = 0x0001;
-const TILESTATE_DEPRECATED = 0x0002;
-const TILESTATE_NOPVP = 0x0004;
-const TILESTATE_NOLOGOUT = 0x0008;
-const TILESTATE_PVPZONE = 0x0010;
-const TILESTATE_REFRESH = 0x0020;
+const HEADERS = require("./lib/headers");
 
 const NODE_ESC = 0xFD;
 const NODE_INIT = 0xFE;
 const NODE_TERM = 0xFF;
 
-__VERSION__ = "0.4.0";
+__VERSION__ = "1.0.0";
 
 function writeOTBM(__OUTFILE__, data) {
 
@@ -78,16 +43,16 @@ function writeOTBM(__OUTFILE__, data) {
      */
 
     switch(node.type) {
-      case "OTBM_TILE_AREA":
+      case HEADERS.OTBM_TILE_AREA:
         return node.tiles;
-      case "OTBM_TILE":
-      case "OTBM_HOUSETILE":
+      case HEADERS.OTBM_TILE:
+      case HEADERS.OTBM_HOUSETILE:
         return node.items;
-      case "OTBM_TOWNS":
+      case HEADERS.OTBM_TOWNS:
         return node.towns;
-      case "OTBM_ITEM":
+      case HEADERS.OTBM_ITEM:
         return node.content;
-      case "OTBM_MAP_DATA":
+      case HEADERS.OTBM_MAP_DATA:
         return node.features;
       default:
         return node.nodes;
@@ -106,59 +71,59 @@ function writeOTBM(__OUTFILE__, data) {
 
     // Write each node type
     switch(node.type) {
-      case "OTBM_MAP_HEADER":
+      case HEADERS.OTBM_MAP_HEADER:
         buffer = Buffer.alloc(17); 
-        buffer.writeUInt8(OTBM_MAP_HEADER, 0);
+        buffer.writeUInt8(HEADERS.OTBM_MAP_HEADER, 0);
         buffer.writeUInt32LE(node.version, 1);
         buffer.writeUInt16LE(node.mapWidth, 5);
         buffer.writeUInt16LE(node.mapHeight, 7);
         buffer.writeUInt32LE(node.itemsMajorVersion, 9);
         buffer.writeUInt32LE(node.itemsMinorVersion, 13);
         break;
-      case "OTBM_MAP_DATA":
+      case HEADERS.OTBM_MAP_DATA:
         buffer = Buffer.alloc(1); 
-        buffer.writeUInt8(OTBM_MAP_DATA, 0);
+        buffer.writeUInt8(HEADERS.OTBM_MAP_DATA, 0);
         buffer = Buffer.concat([buffer, writeAttributes(node)]);
         break;
-      case "OTBM_TILE_AREA":
+      case HEADERS.OTBM_TILE_AREA:
         buffer = Buffer.alloc(6); 
-        buffer.writeUInt8(OTBM_TILE_AREA, 0);
+        buffer.writeUInt8(HEADERS.OTBM_TILE_AREA, 0);
         buffer.writeUInt16LE(node.x, 1);
         buffer.writeUInt16LE(node.y, 3);
         buffer.writeUInt8(node.z, 5);
         break;
-      case "OTBM_TILE":
+      case HEADERS.OTBM_TILE:
         buffer = Buffer.alloc(3); 
-        buffer.writeUInt8(OTBM_TILE, 0);
+        buffer.writeUInt8(HEADERS.OTBM_TILE, 0);
         buffer.writeUInt8(node.x, 1);
         buffer.writeUInt8(node.y, 2);
         buffer = Buffer.concat([buffer, writeAttributes(node)]);
         break;
-      case "OTBM_HOUSETILE":
+      case HEADERS.OTBM_HOUSETILE:
         buffer = Buffer.alloc(7);
-        buffer.writeUInt8(OTBM_HOUSETILE, 0);
+        buffer.writeUInt8(HEADERS.OTBM_HOUSETILE, 0);
         buffer.writeUInt8(node.x, 1);
         buffer.writeUInt8(node.y, 2);
         buffer.writeUInt32LE(node.houseId, 3);
         buffer = Buffer.concat([buffer, writeAttributes(node)]);
         break;
-      case "OTBM_ITEM":
+      case HEADERS.OTBM_ITEM:
         buffer = Buffer.alloc(3); 
-        buffer.writeUInt8(OTBM_ITEM, 0);
+        buffer.writeUInt8(HEADERS.OTBM_ITEM, 0);
         buffer.writeUInt16LE(node.id, 1);
         buffer = Buffer.concat([buffer, writeAttributes(node)]);
         break;
-      case "OTBM_WAYPOINTS":
+      case HEADERS.OTBM_WAYPOINTS:
         buffer = Buffer.alloc(1); 
-        buffer.writeUInt8(OTBM_WAYPOINTS, 0);
+        buffer.writeUInt8(HEADERS.OTBM_WAYPOINTS, 0);
         break;
-      case "OTBM_TOWNS":
+      case HEADERS.OTBM_TOWNS:
         buffer = Buffer.alloc(1);
-        buffer.writeUInt8(OTBM_TOWNS, 0);
+        buffer.writeUInt8(HEADERS.OTBM_TOWNS, 0);
         break;
-      case "OTBM_TOWN":
+      case HEADERS.OTBM_TOWN:
         buffer = Buffer.alloc(7 + node.name.length + 5);
-        buffer.writeUInt8(OTBM_TOWN, 0);
+        buffer.writeUInt8(HEADERS.OTBM_TOWN, 0);
         buffer.writeUInt32LE(node.townid, 1);
         buffer.writeUInt16LE(node.name.length, 5)
         buffer.write(node.name, 7, "ASCII");
@@ -214,7 +179,7 @@ function writeOTBM(__OUTFILE__, data) {
 
     if(node.destination) {
       buffer = Buffer.alloc(6);
-      buffer.writeUInt8(OTBM_ATTR_TELE_DEST);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_TELE_DEST);
       buffer.writeUInt16LE(node.destination.x, 1);
       buffer.writeUInt16LE(node.destination.y, 3);
       buffer.writeUInt8(node.destination.z, 5);
@@ -224,14 +189,14 @@ function writeOTBM(__OUTFILE__, data) {
     // Write description property
     if(node.description) {
       buffer = Buffer.alloc(1);
-      buffer.writeUInt8(OTBM_ATTR_DESCRIPTION, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_DESCRIPTION, 0);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer, writeASCIIString16LE(node.description)])
     }
 
     // Node has an unique identifier
     if(node.uid) {
       buffer = Buffer.alloc(3);
-      buffer.writeUInt8(OTBM_ATTR_UNIQUE_ID, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_UNIQUE_ID, 0);
       buffer.writeUInt16LE(node.uid, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -239,7 +204,7 @@ function writeOTBM(__OUTFILE__, data) {
     // Node has an action identifier
     if(node.aid) {
       buffer = Buffer.alloc(3);
-      buffer.writeUInt8(OTBM_ATTR_ACTION_ID, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_ACTION_ID, 0);
       buffer.writeUInt16LE(node.aid, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -247,7 +212,7 @@ function writeOTBM(__OUTFILE__, data) {
     // Node has rune charges
     if(node.runeCharges) {
       buffer = Buffer.alloc(3);
-      buffer.writeUInt8(OTBM_ATTR_RUNE_CHARGES);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_RUNE_CHARGES);
       buffer.writeUInt16LE(node.runeCharges, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -255,28 +220,28 @@ function writeOTBM(__OUTFILE__, data) {
     // Spawn file
     if(node.spawnfile) {
       buffer = Buffer.alloc(1);
-      buffer.writeUInt8(OTBM_ATTR_EXT_SPAWN_FILE, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_EXT_SPAWN_FILE, 0);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer, writeASCIIString16LE(node.spawnfile)])
     }
 
     // Text attribute
     if(node.text) {
       buffer = Buffer.alloc(1);
-      buffer.writeUInt8(OTBM_ATTR_TEXT, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_TEXT, 0);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer, writeASCIIString16LE(node.text)])
     }
 
     // House file
     if(node.housefile) {
       buffer = Buffer.alloc(1);
-      buffer.writeUInt8(OTBM_ATTR_EXT_HOUSE_FILE, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_EXT_HOUSE_FILE, 0);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer, writeASCIIString16LE(node.housefile)])
     }
 
-    // Write OTBM_ATTR_ITEM
+    // Write HEADERS.OTBM_ATTR_ITEM
     if(node.tileid) {
       buffer = Buffer.alloc(3);
-      buffer.writeUInt8(OTBM_ATTR_ITEM, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_ITEM, 0);
       buffer.writeUInt16LE(node.tileid, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -284,7 +249,7 @@ function writeOTBM(__OUTFILE__, data) {
     // Write node count
     if(node.count) {
       buffer = Buffer.alloc(2);
-      buffer.writeUInt8(OTBM_ATTR_COUNT, 0);
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_COUNT, 0);
       buffer.writeUInt8(node.count, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -292,13 +257,13 @@ function writeOTBM(__OUTFILE__, data) {
     // Write the zone fields
     if(node.zones) {
       buffer = Buffer.alloc(5);
-      buffer.writeUInt8(OTBM_ATTR_TILE_FLAGS, 0);
-      var flags = TILESTATE_NONE;
-      flags |= node.zones.protection && TILESTATE_PROTECTIONZONE;
-      flags |= node.zones.noPVP && TILESTATE_NOPVP;
-      flags |= node.zones.noLogout && TILESTATE_NOLOGOUT;
-      flags |= node.zones.PVPZone && TILESTATE_PVPZONE;
-      flags |= node.zones.refresh && TILESTATE_REFRESH;
+      buffer.writeUInt8(HEADERS.OTBM_ATTR_TILE_FLAGS, 0);
+      var flags = HEADERS.TILESTATE_NONE;
+      flags |= node.zones.protection && HEADERS.TILESTATE_PROTECTIONZONE;
+      flags |= node.zones.noPVP && HEADERS.TILESTATE_NOPVP;
+      flags |= node.zones.noLogout && HEADERS.TILESTATE_NOLOGOUT;
+      flags |= node.zones.PVPZone && HEADERS.TILESTATE_PVPZONE;
+      flags |= node.zones.refresh && HEADERS.TILESTATE_REFRESH;
       buffer.writeUInt32LE(flags, 1);
       attributeBuffer = Buffer.concat([attributeBuffer, buffer]);
     }
@@ -332,8 +297,8 @@ function readOTBM(__INFILE__) {
 
     switch(data.readUInt8(0)) {
 
-      case OTBM_MAP_HEADER:
-        this.type = "OTBM_MAP_HEADER";
+      case HEADERS.OTBM_MAP_HEADER:
+        this.type = HEADERS.OTBM_MAP_HEADER;
         this.version = data.readUInt32LE(1),
         this.mapWidth = data.readUInt16LE(5),
         this.mapHeight = data.readUInt16LE(7),
@@ -342,65 +307,65 @@ function readOTBM(__INFILE__) {
         break;
 
       // High level map data (e.g. areas, towns, and waypoints)
-      case OTBM_MAP_DATA:
-        this.type = "OTBM_MAP_DATA";
+      case HEADERS.OTBM_MAP_DATA:
+        this.type = HEADERS.OTBM_MAP_DATA;
         Object.assign(this, readAttributes(data.slice(1)));
         break;
 
       // A tile area
-      case OTBM_TILE_AREA:
-        this.type = "OTBM_TILE_AREA";
+      case HEADERS.OTBM_TILE_AREA:
+        this.type = HEADERS.OTBM_TILE_AREA;
         this.x = data.readUInt16LE(1);
         this.y = data.readUInt16LE(3);
         this.z = data.readUInt8(5);
         break;
 
       // A specific tile at location inside the parent tile area
-      case OTBM_TILE:
-        this.type = "OTBM_TILE";
+      case HEADERS.OTBM_TILE:
+        this.type = HEADERS.OTBM_TILE;
         this.x = data.readUInt8(1);
         this.y = data.readUInt8(2);
         Object.assign(this, readAttributes(data.slice(3)));
         break;
 
       // A specific item inside the parent tile
-      case OTBM_ITEM:
-        this.type = "OTBM_ITEM";
+      case HEADERS.OTBM_ITEM:
+        this.type = HEADERS.OTBM_ITEM;
         this.id = data.readUInt16LE(1);
         Object.assign(this, readAttributes(data.slice(3)));
         break;
 
-      // Parse OTBM_HOUSETILE entity
-      case OTBM_HOUSETILE:
-        this.type = "OTBM_HOUSETILE";
+      // Parse HEADERS.OTBM_HOUSETILE entity
+      case HEADERS.OTBM_HOUSETILE:
+        this.type = HEADERS.OTBM_HOUSETILE;
         this.x = data.readUInt8(1);
         this.y = data.readUInt8(2);
         this.houseId = data.readUInt32LE(3);
         Object.assign(this, readAttributes(data.slice(7)));
         break;
 
-      // Parse OTBM_WAYPOINTS structure
-      case OTBM_WAYPOINTS:
-        this.type = "OTBM_WAYPOINTS";
+      // Parse HEADERS.OTBM_WAYPOINTS structure
+      case HEADERS.OTBM_WAYPOINTS:
+        this.type = HEADERS.OTBM_WAYPOINTS;
         break;
 
       // Single waypoint entity
-      case OTBM_WAYPOINT:
-        this.type = "OTBM_WAYPOINT";
+      case HEADERS.OTBM_WAYPOINT:
+        this.type = HEADERS.OTBM_WAYPOINT;
         this.name = readASCIIString16LE(data.slice(1));
         this.x = data.readUInt16LE(3 + this.name.length);
         this.y = data.readUInt16LE(5 + this.name.length);
         this.z = data.readUInt8(7 + this.name.length);
         break;
 
-      // Parse OTBM_TOWNS
-      case OTBM_TOWNS:
-        this.type = "OTBM_TOWNS";
+      // Parse HEADERS.OTBM_TOWNS
+      case HEADERS.OTBM_TOWNS:
+        this.type = HEADERS.OTBM_TOWNS;
         break;
 
       // Single town entity
-      case OTBM_TOWN:
-        this.type = "OTBM_TOWN";
+      case HEADERS.OTBM_TOWN:
+        this.type = HEADERS.OTBM_TOWN;
         this.townid = data.readUInt16LE(1);
         // Some two extra bytes ???
         this.name = readASCIIString16LE(data.slice(5));
@@ -455,20 +420,20 @@ function readOTBM(__INFILE__) {
      */
 
     switch(this.type) {
-      case "OTBM_TILE_AREA":
+      case HEADERS.OTBM_TILE_AREA:
         this.tiles = children;
         break;
-      case "OTBM_TILE":
-      case "OTBM_HOUSETILE":
+      case HEADERS.OTBM_TILE:
+      case HEADERS.OTBM_HOUSETILE:
         this.items = children;
         break;
-      case "OTBM_TOWNS":
+      case HEADERS.OTBM_TOWNS:
         this.towns = children;
         break;
-      case "OTBM_ITEM":
+      case HEADERS.OTBM_ITEM:
         this.content = children;
         break;
-      case "OTBM_MAP_DATA":
+      case HEADERS.OTBM_MAP_DATA:
         this.features = children;
         break;
       default:
@@ -507,32 +472,32 @@ function readOTBM(__INFILE__) {
       switch(data.readUInt8(i++)) {
 
         // Text is written
-        case OTBM_ATTR_TEXT:
+        case HEADERS.OTBM_ATTR_TEXT:
           properties.text = readASCIIString16LE(data.slice(i));
           i += properties.text.length + 2;
           break;
 
         // Spawn file name
-        case OTBM_ATTR_EXT_SPAWN_FILE:
+        case HEADERS.OTBM_ATTR_EXT_SPAWN_FILE:
           properties.spawnfile = readASCIIString16LE(data.slice(i));
           i += properties.spawnfile.length + 2;
           break;
 
         // House file name
-        case OTBM_ATTR_EXT_HOUSE_FILE:
+        case HEADERS.OTBM_ATTR_EXT_HOUSE_FILE:
           properties.housefile = readASCIIString16LE(data.slice(i));
           i += properties.housefile.length + 2;
           break;
 
         // House door identifier (1 byte)
-        case OTBM_ATTR_HOUSEDOORID:
+        case HEADERS.OTBM_ATTR_HOUSEDOORID:
           properties.houseDoorId = data.readUInt8(i);
           i += properties.houseDoorId.length + 2;
           break;
 
         // Description is written (N bytes)
         // May be written multiple times
-        case OTBM_ATTR_DESCRIPTION:
+        case HEADERS.OTBM_ATTR_DESCRIPTION:
           var descriptionString = readASCIIString16LE(data.slice(i));
           if(properties.description) {
             properties.description = properties.description + " " + descriptionString;
@@ -543,65 +508,65 @@ function readOTBM(__INFILE__) {
           break;
 
         // Description is written (N bytes)
-        case OTBM_ATTR_DESC:
+        case HEADERS.OTBM_ATTR_DESC:
           properties.text = readASCIIString16LE(data.slice(i));
           i += properties.text.length + 2;
           break;
 
         // Depot identifier (2 byte)
-        case OTBM_ATTR_DEPOT_ID:
+        case HEADERS.OTBM_ATTR_DEPOT_ID:
           properties.depotId = data.readUInt16LE(i);
           i += 2;
           break;
 
         // Tile flags indicating the type of tile (4 Bytes)
-        case OTBM_ATTR_TILE_FLAGS:
+        case HEADERS.OTBM_ATTR_TILE_FLAGS:
           var flags = data.readUInt32LE(i);
 
           // Read individual tile flags using bitwise AND &
           properties.zones = {
-            "protection": flags & TILESTATE_PROTECTIONZONE,
-            "noPVP": flags & TILESTATE_NOPVP,
-            "noLogout": flags & TILESTATE_NOLOGOUT,
-            "PVPZone": flags & TILESTATE_PVPZONE,
-            "refresh": flags & TILESTATE_REFRESH
+            "protection": flags & HEADERS.TILESTATE_PROTECTIONZONE,
+            "noPVP": flags & HEADERS.TILESTATE_NOPVP,
+            "noLogout": flags & HEADERS.TILESTATE_NOLOGOUT,
+            "PVPZone": flags & HEADERS.TILESTATE_PVPZONE,
+            "refresh": flags & HEADERS.TILESTATE_REFRESH
           }
 
           i += 4;
           break;
 
         // N (2 Bytes)
-        case OTBM_ATTR_RUNE_CHARGES:
+        case HEADERS.OTBM_ATTR_RUNE_CHARGES:
           properties.runeCharges = data.readUInt16LE(i);
           i += 2;
           break;
 
         // The item count (1 byte)
-        case OTBM_ATTR_COUNT:
+        case HEADERS.OTBM_ATTR_COUNT:
           properties.count = data.readUInt8(i);
           i++;
           break;
 
         // The main item identifier	(2 bytes)
-        case OTBM_ATTR_ITEM:
+        case HEADERS.OTBM_ATTR_ITEM:
           properties.tileid = data.readUInt16LE(i);
           i += 2;
           break;
 
         // Action identifier was set (2 bytes)
-        case OTBM_ATTR_ACTION_ID:
+        case HEADERS.OTBM_ATTR_ACTION_ID:
           properties.aid = data.readUInt16LE(i);
           i += 2;
           break;
 
         // Unique identifier was set (2 bytes)
-        case OTBM_ATTR_UNIQUE_ID:
+        case HEADERS.OTBM_ATTR_UNIQUE_ID:
           properties.uid = data.readUInt16LE(i);
           i += 2;
           break;
 
         // Teleporter given destination (x, y, z using 2, 2, 1 bytes respectively)
-        case OTBM_ATTR_TELE_DEST:
+        case HEADERS.OTBM_ATTR_TELE_DEST:
           properties.destination = {
             "x": data.readUInt16LE(i),
             "y": data.readUInt16LE(i + 2),
@@ -694,3 +659,5 @@ function readOTBM(__INFILE__) {
 
 module.exports.read = readOTBM;
 module.exports.write = writeOTBM;
+module.exports.HEADERS = HEADERS;
+module.exports.__VERSION__ = __VERSION__;
