3661298c51b10adfda4524b863dcfc20d10f9f38
Forby <38814434+Inconcessus@users.noreply.github.com>
2018-05-17 11:26:06 +0200

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -17,13 +17,13 @@ mapData.data.nodes.forEach(function(x) {
   x.features.forEach(function(x) {
     
     // Skip anything that is not a tile area
-    if(x.type !== "OTBM_TILE_AREA") return; 
+    if(x.type !== otbm2json.HEADERS.OTBM_TILE_AREA) return; 
 
     // For each tile area; go over all actual tiles
     x.tiles.forEach(function(x) {
 
       // Skip anything that is not a tile (e.g. house tiles)
-      if(x.type !== "OTBM_TILE") return; 
+      if(x.type !== otbm2json.HEADERS.OTBM_TILE) return; 
 
       // Create a chessboard pattern using bitwise operators
       // Replace the id property of each tile
